320f4a2b3083f2116b3b89607de58db96b53e9c4
nirmal4938 <kumarnirmal9330@gmail.com>
2025-03-03 16:13:34 +0530

    added logging purpose console in jest.setup.js

--- jest.setup.js
+++ jest.setup.js
@@ -5,6 +5,11 @@ const mongoose = require('mongoose');
 
 beforeAll(async () => {
     console.log('Connecting to MongoDB...');
+    console.log('MONGO_URI:', process.env.MONGO_URI);
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI is not set!');
+    }
+
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
